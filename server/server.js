27eb292a03a3f9f655ff3b047208b78a7a22444a
akojha556 <107338576+akojha556@users.noreply.github.com>
2025-12-20 15:57:47 +0530

    Setup MVC architecture

--- server/server.js
+++ server/server.js
@@ -1,16 +1,7 @@
 require("dotenv").config();
-const express = require("express");
+const app = require("./src/app");
 
-const app = express();
 const PORT = process.env.PORT || 5000;
-
-// Middleware
-app.use(express.json());
-
-// Test route
-app.get("/", (req, res) => {
-     res.send("Backend running");
-});
 
 // Start server
 app.listen(PORT, () => {
