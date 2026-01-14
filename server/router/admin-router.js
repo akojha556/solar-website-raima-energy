cc4a59da86e2cafb952c377d6c04d8a88118d717
akojha556 <107338576+akojha556@users.noreply.github.com>
2026-01-14 20:10:35 +0530

    add rate limiting to api/admin routes

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { loginAdmin, forgetPassword, resetPassword } from "../controller/admin-controller.js";
+import { limiter } from "../middleware/rateLimiter.js";
 
 export const adminRouter = express.Router();
 
-adminRouter.post("/admin-login", loginAdmin);
-adminRouter.post("/forget-password", forgetPassword);
-adminRouter.post("/reset-password/:token", resetPassword);
+adminRouter.post("/admin-login", limiter, loginAdmin);
+adminRouter.post("/forget-password", limiter, forgetPassword);
+adminRouter.post("/reset-password/:token", limiter, resetPassword);
